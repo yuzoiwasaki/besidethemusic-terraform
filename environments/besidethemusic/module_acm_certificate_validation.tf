c9bc474a3c4f08a8f27c293e150b38515ae2c494
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-16 15:19:04 +0900

    Some updates

---
+++ environments/besidethemusic/module_acm_certificate_validation.tf
@@ -0,0 +1,8 @@
+module "acm_certificate_validation_besidethemusic_tokyo" {
+  source          = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//acm_certificate_validation"
+  acm_region      = "us-east-1"
+  certificate_arn = module.acm_certificate_besidethemusic_tokyo.acm_certificate_arn
+  validation_record_fqdns = [
+    module.route53_record_cloudfront_besidethemusic_tokyo_acm_1.route53_record_fqdn,
+  ]
+}
---
+++ environments/besidethemusic/module_route53_record.tf
@@ -0,0 +1,21 @@
+#####################################
+# For besidethemusic.tokyo
+#####################################
+
+module "route53_record_cloudfront_besidethemusic_tokyo_acm_1" {
+  source  = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//route53_record"
+  zone_id = module.route53_zone_app.route53_zone_id
+  name = lookup(
+    module.acm_certificate_besidethemusic_tokyo.acm_certificate_domain_validation_options[0],
+    "resource_record_name"
+  )
+  type = lookup(
+    module.acm_certificate_besidethemusic_tokyo.acm_certificate_domain_validation_options[0],
+    "resource_record_type"
+  )
+  records = [lookup(
+    module.acm_certificate_besidethemusic_tokyo.acm_certificate_domain_validation_options[0],
+    "resource_record_value"
+  )]
+  ttl = 60
+}
