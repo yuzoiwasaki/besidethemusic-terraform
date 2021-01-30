9578e384cffc721a2f6e53ceef83b4dfb6bbc3e3
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-01-30 12:10:43 +0900

    Fix

--- environments/besidethemusic/module_route53_record_alias.tf
+++ environments/besidethemusic/module_route53_record_alias.tf
@@ -1,24 +1,7 @@
-variable "route53_zone_alias_besidethemusic_tokyo_cloudfront" {
-  type = map(string)
-
-  default = {
-    "default.name"        = "besidethemusic.tokyo"
-    "default.domain_name" = "d340wkke3eba05.cloudfront.net"
-  }
-}
-
 module "route53_record_alias_besidethemusic_tokyo_cloudfront" {
-  source  = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//route53_record_alias"
-  zone_id = module.route53_zone_app.route53_zone_id
-  name = lookup(
-    var.route53_zone_alias_besidethemusic_tokyo_cloudfront,
-    "${terraform.workspace}.name",
-    var.route53_zone_alias_besidethemusic_tokyo_cloudfront["default.name"],
-  )
-  alias_name = lookup(
-    var.route53_zone_alias_besidethemusic_tokyo_cloudfront,
-    "${terraform.workspace}.domain_name",
-    var.route53_zone_alias_besidethemusic_tokyo_cloudfront["default.domain_name"],
-  )
+  source        = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//route53_record_alias"
+  zone_id       = module.route53_zone_app.route53_zone_id
+  name          = "besidethemusic.tokyo"
+  alias_name    = "d340wkke3eba05.cloudfront.net"
   alias_zone_id = "Z2FDTNDATAQYW2"
 }
--- environments/besidethemusic/module_route53_zone.tf
+++ environments/besidethemusic/module_route53_zone.tf
@@ -1,16 +1,4 @@
-variable "route53_zone_app" {
-  type = map(string)
-
-  default = {
-    "default.name" = "besidethemusic.tokyo"
-  }
-}
-
 module "route53_zone_app" {
   source = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//route53_zone"
-  name = lookup(
-    var.route53_zone_app,
-    "${terraform.workspace}.name",
-    var.route53_zone_app["default.name"],
-  )
+  name   = "besidethemusic.tokyo"
 }
