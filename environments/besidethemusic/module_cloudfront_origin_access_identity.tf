ce903a73e1bef183479e8660c890c88e22dcb254
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-01-30 12:06:18 +0900

    Stop passing on map

--- environments/besidethemusic/module_cloudfront_origin_access_identity.tf
+++ environments/besidethemusic/module_cloudfront_origin_access_identity.tf
@@ -1,16 +1,4 @@
-variable "cloudfront_origin_access_identity_app" {
-  type = map(string)
-
-  default = {
-    "default.comment" = "access-identity-besidethemusic"
-  }
-}
-
 module "cloudfront_origin_access_identity_app" {
   source = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//cloudfront_origin_access_identity"
-  comment = "${lookup(
-    var.cloudfront_origin_access_identity_app,
-    "${terraform.workspace}.comment",
-    var.cloudfront_origin_access_identity_app["default.comment"],
-  )}-${terraform.workspace}-bucket"
+  comment = "access-identity-besidethemusic-${terraform.workspace}-bucket"
 }
