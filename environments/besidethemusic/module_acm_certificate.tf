0e897e6741d08ab5f5725b4741c7a16b42409cde
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-01-30 12:08:54 +0900

    Fix

--- environments/besidethemusic/module_acm_certificate.tf
+++ environments/besidethemusic/module_acm_certificate.tf
@@ -1,26 +1,8 @@
-variable "acm_certificate_besidethemusic_tokyo" {
-  type = map(string)
-
-  default = {
-    "default.acm_region"  = "us-east-1"
-    "default.domain_name" = "besidethemusic.tokyo"
-  }
-}
-
 module "acm_certificate_besidethemusic_tokyo" {
   source = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//acm_certificate"
 
-  acm_region = lookup(
-    var.acm_certificate_besidethemusic_tokyo,
-    "${terraform.workspace}.acm_region",
-    var.acm_certificate_besidethemusic_tokyo["default.acm_region"],
-  )
-
-  domain_name = lookup(
-    var.acm_certificate_besidethemusic_tokyo,
-    "${terraform.workspace}.domain_name",
-    var.acm_certificate_besidethemusic_tokyo["default.domain_name"],
-  )
+  acm_region  = "us-east-1"
+  domain_name = "besidethemusic.tokyo"
 
   tags = {
     "Environment" = terraform.workspace
--- environments/besidethemusic/module_cloudfront_origin_access_identity.tf
+++ environments/besidethemusic/module_cloudfront_origin_access_identity.tf
@@ -1,4 +1,4 @@
 module "cloudfront_origin_access_identity_app" {
-  source = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//cloudfront_origin_access_identity"
+  source  = "git@github.com:yuzoiwasaki/aws-terraform-modules.git//cloudfront_origin_access_identity"
   comment = "access-identity-besidethemusic-${terraform.workspace}-bucket"
 }
