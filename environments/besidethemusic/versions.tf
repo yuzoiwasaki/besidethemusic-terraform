c34cfda9c68ae7faf55c6f34f1851772b3f2f37c
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2022-02-26 09:52:22 +0900

    terraform fmt

--- environments/besidethemusic/versions.tf
+++ environments/besidethemusic/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 2.45.0"
     }
     template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/template"
       version = "~> 2.1.2"
     }
   }
